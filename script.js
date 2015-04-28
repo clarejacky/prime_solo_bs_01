fdfea7a7f3b50dc5171a898a566af6f536816ced
clarejacky <clare.jacky@gmail.com>
2015-04-28 12:10:03 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-var apikey = ''; // Put your API key here
+var apikey = '91df4711deb6a6fa8e7c694207940394218396cd'; // Put your API key here
 
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
     console.log(results);
+     for(var i=0; i < 9; i++) {
+    	$(".mainContent").append("<div id='" + results[i].id + "'class='col-md-4 well second'><p class='lead'>"+results[i].name+"</p><div class='hidden-sm hidden-xs'><img id='icon' src='"+results[i].image.icon_url+"'/></div><div>Description: "+results[i].deck+"</div><button class='btn-sm btn-success remove'></button></div>");
+
+    	$(".mainContent").on("click", "button", function(){
+    		$(this).parent().remove();
+    	});
+    	// $(".mainContent").on("click", "#"+results[i].id, function(){
+    	// $(this).children().hide();
+    	// $(this).siblings().children("#game").hide();
+    	// $(this).children().slideDown();
+
+    	// });
+    	}
 }
 
 $(document).ready(function() {
 
 	// Start the search here!
+	search("batman");
+
 	
 });
 
